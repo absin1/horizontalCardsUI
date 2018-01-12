112f045736e13d625758743b4c71a962b5bd7e69
Abhinav <abhinav1@istarindia.com>
2018-01-12 11:04:03 +0530

    1st

--- horizontalCardsUI/ViewController.swift
+++ horizontalCardsUI/ViewController.swift
@@ -8,9 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    @IBOutlet weak var cards: UICollectionView!
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = cards.dequeueReusableCell(withReuseIdentifier: "quote", for: indexPath)
+        return cell
+    }
+    
 
     override func viewDidLoad() {
+        cards.delegate = self
+        cards.dataSource = self
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -20,6 +32,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let pageWidth:Float = Float(scrollView.frame.width)
+        let currentOffset:Float = Float(scrollView.contentOffset.x)
+        print(currentOffset)
+        let targetOffset:Float = Float(targetContentOffset.pointee.x)
+        print(targetOffset)
+        var newTargetOffset:Float = 0
+        if(targetOffset>currentOffset){
+            newTargetOffset = ceilf(currentOffset/pageWidth)*pageWidth;
+        } else {
+            newTargetOffset = floorf(currentOffset/pageWidth)*pageWidth
+        }
+        if(newTargetOffset<0){
+            newTargetOffset = 0;
+        } else if(newTargetOffset>Float(scrollView.contentSize.width)){
+            newTargetOffset = Float(scrollView.contentSize.width)
+        }
+        targetContentOffset.pointee.x = CGFloat(currentOffset)
+        scrollView.setContentOffset(CGPoint(x: CGFloat(newTargetOffset), y: 0), animated: true)
+    }
 }
 
